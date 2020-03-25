3c21194eb4676c438904e77c5a84f98b29544b16
Bryan Deetz <concise.constructs@gmail.com>
2020-03-25 13:23:15 -0500

    Added linking logic

--- UpdateHandler.class.ts
+++ UpdateHandler.class.ts
@@ -22,6 +22,7 @@ export abstract class UpdateHandler extends LambdaHandler {
         protected hookConstructorPre() {
           this.requiredInputs = ['item']
           this.needsToConnectToDatabase = true
+          this.needsToInvokeLambdas = true
         }
 
 
@@ -32,10 +33,84 @@ export abstract class UpdateHandler extends LambdaHandler {
 
 
     protected performActions() {
+      this.unlinkOldRecords()
+      this.linkNewRecords()
       this.db.put(this.makePutSyntax()).promise()
         .then(result => this.hasSucceeded(result))
         .catch(error => this.hasFailed(error))
     }
+
+
+
+
+        private unlinkOldRecords() {
+          if (!this.request.item.links._unlink) return
+          for (let [ table, ids ] of Object.entries(this.request.item.links._unlink)) {
+            Object.keys(ids).forEach(id => this.unlink({ table: table, id: id }))
+          }
+          delete this.request.item.links._unlink
+        }
+
+
+
+
+            private unlink(params) {
+              this.lambda.invoke({
+                FunctionName: `Database-${ process.env.stage }-links-unlink`,
+                Payload: JSON.stringify(this.makeUnlinkPayload(params))
+              }).promise()
+            }
+
+
+
+
+                private makeUnlinkPayload(params:{ table:string, id:string }) {
+                  return {
+                    silo: `${ process.env.saasName }-${ process.env.stage }`,
+                    table: params.table,
+                    id: params.id,
+                    foreignTable: `${ process.env.modelName }`,
+                    foreignId: this.request.item.id,
+                    accountId: `${ this.request.item.table.split('.')[0] }`,
+                    link: true
+                  }
+                }
+
+
+
+
+        private linkNewRecords() {
+          if (!this.request.item.links.link) return
+          for (let [ table, ids ] of Object.entries(this.request.item.links.link)) {
+            Object.keys(ids).forEach(id => this.link({ table: table, id: id }))
+          }
+          delete this.request.item.links.link
+        }
+
+
+
+
+            private link(params:{ table:string, id:string }) {
+              this.lambda.invoke({
+                FunctionName: `Database-${ process.env.stage }-links-link`,
+                Payload: JSON.stringify(this.makeLinkPayload(params))
+              }).promise()
+            }
+
+
+
+
+                private makeLinkPayload(params) {
+                  return {
+                    silo: `${ process.env.saasName }-${ process.env.stage }`,
+                    table: params.table,
+                    id: params.id,
+                    foreignTable: `${ process.env.modelName }`,
+                    foreignId: this.request.item.id,
+                    accountId: `${ this.request.item.table.split('.')[0] }`,
+                    link: true
+                  }
+                }
 
 
 
